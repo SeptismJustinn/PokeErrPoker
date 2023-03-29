50beb5287d05f77866dcd896ba991ba4c20c2d30
SeptismJustinn <81621295+SeptismJustinn@users.noreply.github.com>
2023-03-29 15:05:21 +0800

    Added updateHealth function
    -Implemented updateHealth function of character class. Allows characters to accept positive or negative numbers to add/deduct to their current health.
    -This function updates both #health-bar's width and #health-counter's innerText.
    -If currHealth reaches 0, healthBar.style.width and healthCounter is set to 0 and character.alive is set to false.
    -To do: Implement code to handle dead characters

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ class Character {
     this.healthCounter = healthCounter;
     // Boolean to check if is human or computer.
     this.player = player;
+    // Stores initial health as max health as characters should be generated with full health.
+    this.maxHealth = Number(this.healthCounter.innerText);
+    // Each character generated as alive.
+    this.alive = true;
+  }
+
+  updateHealth(amt) {
+    const currHealth = Number(this.healthCounter.innerText) + amt;
+    if (currHealth > 0) {
+      this.healthCounter.innerText = currHealth;
+      this.healthBar.style.width = `${Math.round(
+        (currHealth / this.maxHealth) * 100
+      )}%`;
+    } else {
+      this.healthCounter.innerText = 0;
+      this.healthBar.style.width = 0;
+      this.alive = false;
+      // Insert ded function?
+    }
   }
 }
 
